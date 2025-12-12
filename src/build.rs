6f209ee7f312bf2fbc0ad6e4c07f311489b8700a
PitchBlackNights <pitchblacknights@proton.me>
2025-12-12 00:42:18 -0600

    Moved JVM container & Proc Macros to `oxidizemc` + Created ModLoaderConfig for FabricMC

--- src/build.rs
+++ src/build.rs
@@ -1,17 +1,7 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
+use oxidizemc::build::ModLoaderConfig;
+
+pub const FABRICMC_MOD_LOADER: ModLoaderConfig = ModLoaderConfig {
+    name: "FabricMC",
+    lib_name: "oxidizemc-fabric",
+    mod_initializers: &["ModInitializer", "ClientModInitializer", "DedicatedServerModInitializer"],
 };
-use toml::Table;
-
-pub fn configure() {
-    let manifest_path: PathBuf =
-        Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("Cargo.toml");
-    let manifest: String = fs::read_to_string(manifest_path).unwrap();
-    let toml: Table = manifest.parse().unwrap();
-    let java_package: &str = toml["package"]["metadata"]["java-package"]
-        .as_str()
-        .expect("Cargo Manifest is missing required property `package.metadata.java-package`");
-
-    println!("cargo:rustc-env=JAVA_PACKAGE={}", java_package);
-}
--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,19 @@ pub mod sys {
     pub use oxidizemc_fabric_bindings::*;
     pub type __Env<'a> = java_oxide::Env<'a>;
 }
-#[cfg(feature = "derive")]
-pub mod derive {
-    pub use oxidizemc_fabric_derive::*;
-}
 #[cfg(feature = "build")]
 pub mod build;
-pub(crate) mod util;
 
 use self::{
-    sys::{java::lang::String as JString, *},
-    util::VM,
+    sys::{java::lang::String as JString, *}
 };
 use java_oxide::{Env, Global, Local};
-
-#[doc(hidden)]
-pub(crate) static __VM: VM = VM::new();
-
-#[doc(hidden)]
-pub fn __java_entrypoint<F: FnOnce()>(env: Env, user_main: F) {
-    __VM.init(env.vm());
-    user_main()
-}
+use oxidizemc::JVM;
 
 pub struct LoggerFactory {}
 impl LoggerFactory {
     pub fn get_logger(name: &str) -> JLogger {
-        __VM.get().with_env(|env: Env| {
+        JVM.with_env(|env: Env| {
             let logger: Local<org::slf4j::Logger> =
                 org::slf4j::LoggerFactory::getLogger_String(env, JString::from_env_str(env, name))
                     .expect("Exception was thrown")
@@ -47,7 +33,7 @@ pub struct JLogger {
 }
 impl JLogger {
     pub fn get_name(&self) -> String {
-        self.inner.vm().with_env(|env| {
+        JVM.with_env(|env| {
             let logger: Local<org::slf4j::Logger> = self.inner.as_local(env);
             logger
                 .getName()
@@ -58,7 +44,7 @@ impl JLogger {
     }
 
     pub fn info(&self, text: String) {
-        self.inner.vm().with_env(|env| {
+        JVM.with_env(|env| {
             let logger: Local<org::slf4j::Logger> = self.inner.as_local(env);
             logger
                 .info_String(JString::from_env_str(env, text))
